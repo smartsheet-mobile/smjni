af288d773a7babd308364785bc7b87b13cad27a0
Eugene Gershnik <gershnik@hotmail.com>
2019-09-29 10:57:13 -0700

    Finalizing copyright

--- inc/smjni/config.h
+++ inc/smjni/config.h
@@ -1,6 +1,5 @@
 /*
- See the NOTICE file distributed with this work for additional 
- information regarding copyright ownership.
+ Copyright 2019 SmJNI Contributors
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
--- jnigen/build.gradle
+++ jnigen/build.gradle
@@ -1,4 +1,7 @@
 /*
+ Copyright 2014 Smartsheet Inc.
+ Copyright 2019 SmJNI Contributors
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
