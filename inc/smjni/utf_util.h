aaee698a68aaee134b49c9e2ba83a535b2379fd2
Eugene Gershnik <gershnik@hotmail.com>
2020-10-16 22:21:39 -0700

    Better UTF-8 decoding

--- inc/smjni/utf_util.h
+++ inc/smjni/utf_util.h
@@ -12,7 +12,31 @@
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  See the License for the specific language governing permissions and
  limitations under the License.
+
+ 
+ Portions Copyright (c) 2008-2010 Björn Höhrmann <bjoern@hoehrmann.de>.
+ See http://bjoern.hoehrmann.de/utf-8/decoder/dfa/ for details.
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 */
+
 
 #ifndef HEADER_UTF_UTIL_INCLUDED
 #define HEADER_UTF_UTIL_INCLUDED
@@ -80,92 +104,126 @@ namespace smjni
         }
         return dest;
     }
+
+
+    class utf8_codepoint_decoder
+    {
+    public:
+        constexpr void put(uint32_t byte) noexcept
+        {
+            uint32_t type = s_state_table[byte];
+
+            m_value = (m_state != state_accept) ?
+                    (byte & 0x3fu) | (m_value << 6) :
+                    (0xff >> type) & (byte);
+
+            m_state = s_state_table[256 + m_state + type];
+        }
+        
+        constexpr bool done() const noexcept
+            { return m_state == state_accept; }
+        
+        constexpr bool error() const noexcept
+            { return m_state == state_reject; }
+        
+        constexpr uint32_t value() const noexcept
+            { return m_value; }
+    private:
+        uint32_t m_state = state_accept;
+        uint32_t m_value = 0;
+        
+        static constexpr uint32_t state_accept = 0;
+        static constexpr uint32_t state_reject = 12;
+        
+        static constexpr const uint8_t s_state_table[] = {
+            // The first part of the table maps bytes to character classes that
+            // to reduce the size of the transition table and create bitmasks.
+             0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+             0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+             0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+             0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+             1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,  9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
+             7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,  7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,
+             8,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,  2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+            10,3,3,3,3,3,3,3,3,3,3,3,3,4,3,3, 11,6,6,6,5,8,8,8,8,8,8,8,8,8,8,8,
+
+            // The second part is a transition table that maps a combination
+            // of a state of the automaton and a character class to a state.
+             0,12,24,36,60,96,84,12,12,12,48,72, 12,12,12,12,12,12,12,12,12,12,12,12,
+            12, 0,12,12,12,12,12, 0,12, 0,12,12, 12,24,12,12,12,12,12,24,12,24,12,12,
+            12,12,12,12,12,12,12,24,12,12,12,12, 12,24,12,12,12,12,12,12,12,24,12,12,
+            12,12,12,12,12,12,12,36,12,36,12,12, 12,36,12,12,12,12,12,36,12,36,12,12,
+            12,36,12,12,12,12,12,12,12,12,12,12,
+        };
+    };
+
     
     template<typename InIt, typename Out>
     Out utf8_to_utf16(InIt first, InIt last, Out dest)
     {
-        while(first != last)
+        if (first == last)
+            return dest;
+        
+        uint8_t byte = *first++;
+        
+        for( ; ; )
         {
-            uint8_t lead = *first++;
+            uint32_t value;
             
-        resync:
-            if (lead <= 0b01111111)
+            bool repeat_prev = false;
+            if (byte <= 0x7f)
             {
-                *dest++ = char16_t(lead);
-                continue;
-            }
-            if (lead <= 0b11000001)
-            {
-                *dest++ = u'\uFFFD';
-                continue;
-            }
-            
-            int tail_len;
-            uint8_t tail_lower_bound = 0b10000000;
-            uint8_t tail_upper_bound = 0b10111111;
-            if (lead <= 0b11011111)
-            {
-                tail_len = 1;
-                lead &= 0b00011111;
-            }
-            else if (lead <= 0b11101111)
-            {
-                tail_len = 2;
-                
-                if (lead == 0b11100000)
-                    tail_lower_bound = 0b10100000;
-                else if (lead == 0b11101101)
-                    tail_upper_bound = 0b10011111;
-                
-                lead &= 0b00001111;
-            }
-            else if (lead <= 0b11110100)
-            {
-                tail_len = 3;
-                
-                if (lead == 0b11110000)
-                    tail_lower_bound = 0b10010000;
-                else if (lead == 0b11110100)
-                    tail_upper_bound = 0b10001111;
-                
-                lead &= 0b00000111;
+                value = byte;
             }
             else
             {
-                *dest++ = u'\uFFFD';
-                continue;
-            }
-    
-            uint32_t c = uint32_t(lead);
-            for(int i = 0; i < tail_len; ++i)
-            {
-                if (first == last)
+                utf8_codepoint_decoder decoder;
+                uint32_t first_byte = true;
+                for ( ; ; )
                 {
-                    *dest++ = u'\uFFFD';
-                    return dest;
+                    decoder.put(byte);
+                    
+                    if (decoder.done())
+                    {
+                        value = decoder.value();
+                        break;
+                    }
+                    
+                    if (decoder.error())
+                    {
+                        value = U'\uFFFD';
+                        repeat_prev = !first_byte;
+                        break;
+                    }
+                    if (first == last)
+                    {
+                        value = u'\uFFFD';
+                        break;
+                    }
+                    
+                    byte = *first++;
+                    first_byte = false;
                 }
-                uint8_t tail = *first++;
-                if (tail < tail_lower_bound || tail > tail_upper_bound)
-                {
-                    *dest++ = u'\uFFFD';
-                    lead = tail;
-                    goto resync;
-                }
-                c = (c << 6) | (tail & 0b00111111);
-                tail_lower_bound = 0b10000000;
-                tail_upper_bound = 0b10111111;
             }
-            
-            if (c >= 0x010000)
+                
+            if (value >= 0x010000)
             {
-                c -= 0x010000;
-                *dest++ = char16_t((c >> 10)    + 0x00D800);
-                *dest++ = char16_t((c & 0x03FF) + 0x00DC00);
+                value -= 0x010000;
+                *dest++ = char16_t((value >> 10)    + 0x00D800);
+                *dest++ = char16_t((value & 0x03FF) + 0x00DC00);
             }
             else
             {
-                *dest++ = char16_t(c);
+                *dest++ = char16_t(value);
             }
+            
+            if (repeat_prev)
+                continue;
+            
+            if (first == last)
+                break;
+                
+            byte = *first++;
         }
         
         return dest;
---
+++ tests/src/cpp/utf_tests.cpp
@@ -0,0 +1,214 @@
+/*
+ Copyright 2019 SmJNI Contributors
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+#include <smjni/smjni.h>
+
+#include "catch.hpp"
+
+using namespace smjni;
+
+static std::u16string convert(const char * utf8)
+{
+    std::u16string ret;
+    utf8_to_utf16(utf8, utf8 + strlen(utf8), std::back_inserter(ret));
+    return ret;
+}
+
+static std::string convert(const char16_t * utf16)
+{
+    std::string ret;
+    utf16_to_utf8(utf16, utf16 + std::char_traits<char16_t>::length(utf16), std::back_inserter(ret));
+    return ret;
+}
+
+static std::u32string convert32(const char16_t * utf16)
+{
+    std::u32string ret;
+    utf16_to_utf32(utf16, utf16 + std::char_traits<char16_t>::length(utf16), std::back_inserter(ret));
+    return ret;
+}
+
+static std::u16string convert32(const char32_t * utf32)
+{
+    std::u16string ret;
+    utf32_to_utf16(utf32, utf32 + std::char_traits<char32_t>::length(utf32), std::back_inserter(ret));
+    return ret;
+}
+
+TEST_CASE( "utf8 to utf16", "[utf]" )
+{
+    //Adapted from https://www.w3.org/2001/06/utf-8-wrong/UTF-8-test.html
+        
+    //1  Some correct UTF-8 text
+    CHECK(convert("κόσμε") == u"κόσμε");
+
+    //2  Boundary condition test cases
+        
+    //2.1  First possible sequence of a certain length
+    CHECK(convert("\xC2\x80") == u"\u0080");
+    CHECK(convert("\xE0\xA0\x80") == u"ࠀ");
+    CHECK(convert("\xF0\x90\x80\x80") == u"𐀀");
+    CHECK(convert("\xF8\x88\x80\x80\x80") == u"�����");
+    CHECK(convert("\xFC\x84\x80\x80\x80\x80") == u"������");
+    
+    //2.2  Last possible sequence of a certain length
+    CHECK(convert("\x7F") == u"\u007F");
+    CHECK(convert("\xDF\xBF") == u"\u07FF");
+    CHECK(convert("\xEF\xBF\xBF") == u"\uFFFF");
+    CHECK(convert("\xF7\xBF\xBF\xBF") == u"����");
+    CHECK(convert("\xFB\xBF\xBF\xBF\xBF") == u"�����");
+    CHECK(convert("\xFD\xBF\xBF\xBF\xBF\xBF") == u"������");
+    
+    //2.3  Other boundary conditions
+    CHECK(convert("\xED\x9F\xBF") == u"\uD7FF");
+    CHECK(convert("\xEE\x80\x80") == u"\uE000");
+    CHECK(convert("\xEF\xBF\xBD") == u"�");
+    CHECK(convert("\xF4\x8F\xBF\xBF") == u"\U0010FFFF");
+    CHECK(convert("\xF4\x90\x80\x80") == u"����");
+    
+    //3  Malformed sequences
+    
+    //3.1  Unexpected continuation bytes
+    CHECK(convert("\x80") == u"�");
+    CHECK(convert("\xBF") == u"�");
+    CHECK(convert("\x80\xBF") == u"��");
+    CHECK(convert("\x80\xBF\x80") == u"���");
+    CHECK(convert("\x80\xBF\x80\xBF") == u"����");
+    CHECK(convert("\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F"
+                  "\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F"
+                  "\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF"
+                  "\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF")
+          == u"����������������"
+             u"����������������"
+             u"����������������"
+             u"����������������");
+    
+    //3.2  Lonely start characters
+    CHECK(convert("\xC0 \xC1 \xC2 \xC3 \xC4 \xC5 \xC6 \xC7 \xC8 \xC9 \xCA \xCB \xCC \xCD \xCE \xCF "
+                  "\xD0 \xD1 \xD2 \xD3 \xD4 \xD5 \xD6 \xD7 \xD8 \xD9 \xDA \xDB \xDC \xDD \xDE \xDF ")
+        == u"� � � � � � � � � � � � � � � � "
+           u"� � � � � � � � � � � � � � � � ");
+    CHECK(convert("\xE0 \xE1 \xE2 \xE3 \xE4 \xE5 \xE6 \xE7 \xE8 \xE9 \xEA \xEB \xEC \xED \xEE \xEF ")
+          == u"� � � � � � � � � � � � � � � � ");
+    CHECK(convert("\xF0 \xF1 \xF2 \xF3 \xF4 \xF5 \xF6 \xF7 ") == u"� � � � � � � � ");
+    CHECK(convert("\xF8 \xF9 \xFA \xFB ") == u"� � � � ");
+    CHECK(convert("\xFC \xFD ") == u"� � ");
+    
+    //3.3  Sequences with last continuation byte missing
+    CHECK(convert("\xC0") == u"�");
+    CHECK(convert("\xE0\x80") == u"��");
+    CHECK(convert("\xF0\x80\x80") == u"���");
+    CHECK(convert("\xDF") == u"�");
+    CHECK(convert("\xEF\xBF") == u"�"); //Safari 11.0.1 (as of 11/24/2017 gets this one wrong, Chrome and FF are right
+    CHECK(convert("\xF7\xBF\xBF") == u"���");
+    
+    //3.4  Concatenation of incomplete sequences
+    CHECK(convert("\xC0\xE0\x80\xF0\x80\x80\xDF\xEF\xBF\xF7\xBF\xBF") == u"�����������");
+    
+    //3.5  Impossible bytes
+    CHECK(convert("\xFE") == u"�");
+    CHECK(convert("\xFF") == u"�");
+    CHECK(convert("\xFE\xFE\xFF\xFF") == u"����");
+    
+    //4  Overlong sequences
+    
+    //4.1  Examples of an overlong ASCII character
+    CHECK(convert("\xC0\xAF") == u"��");
+    CHECK(convert("\xE0\x80\xAF") == u"���");
+    CHECK(convert("\xF0\x80\x80\xAF") == u"����");
+    
+    //4.2  Maximum overlong sequences
+    CHECK(convert("\xC1\xBF") == u"��");
+    CHECK(convert("\xE0\x9F\xBF") == u"���");
+    CHECK(convert("\xF0\x8F\xBF\xBF") == u"����");
+    
+    //4.3  Overlong representation of the NUL character
+    CHECK(convert("\xC0\x80") == u"��");
+    CHECK(convert("\xE0\x80\x80") == u"���");
+    CHECK(convert("\xF0\x80\x80\x80") == u"����");
+    
+    //5  Illegal code positions
+    CHECK(convert("\xED\xA0\x80") == u"���");
+    CHECK(convert("\xED\xAD\xBF") == u"���");
+    CHECK(convert("\xED\xAE\x80") == u"���");
+    CHECK(convert("\xED\xAF\xBF") == u"���");
+    CHECK(convert("\xED\xB0\x80") == u"���");
+    CHECK(convert("\xED\xBE\x80") == u"���");
+    CHECK(convert("\xED\xBF\xBF") == u"���");
+    
+    //5.2 Paired UTF-16 surrogates
+    CHECK(convert("\xED\xA0\x80\xED\xB0\x80") == u"������");
+    CHECK(convert("\xED\xA0\x80\xED\xBF\xBF") == u"������");
+    CHECK(convert("\xED\xAD\xBF\xED\xB0\x80") == u"������");
+    CHECK(convert("\xED\xAD\xBF\xED\xBF\xBF") == u"������");
+    CHECK(convert("\xED\xAE\x80\xED\xB0\x80") == u"������");
+    CHECK(convert("\xED\xAE\x80\xED\xBF\xBF") == u"������");
+    CHECK(convert("\xED\xAF\xBF\xED\xB0\x80") == u"������");
+    CHECK(convert("\xED\xAF\xBF\xED\xBF\xBF") == u"������");
+    
+    //5.3 Other illegal code positions
+    CHECK(convert("\xEF\xBF\xBE") == u"\uFFFE");
+    CHECK(convert("\xEF\xBF\xBF") == u"\uFFFF");
+    
+    //From https://www.unicode.org/versions/Unicode10.0.0/ch03.pdf
+    CHECK(convert("\x61\xF1\x80\x80\xE1\x80\xC2\x62\x80\x63\x80\xBF\x64") == u"a���b�c��d");
+
+}
+
+TEST_CASE( "utf16 to utf8", "[utf]" )
+{
+    CHECK(convert(u"κόσμε") == u8"κόσμε");
+    CHECK(convert(u"\xD800") == u8"�");
+    CHECK(convert(u"\xDBFF") == u8"�");
+    CHECK(convert(u"\xDC00") == u8"�");
+    CHECK(convert(u"\xDC00") == u8"�");
+    CHECK(convert(u"\xDFFF") == u8"�");
+    CHECK(convert(u"\xD800\xDBFF") == u8"��");
+    CHECK(convert(u"\xD800\x0061") == u8"�a");
+    CHECK(convert(u"\xDBFF\xDBFF") == u8"��");
+    CHECK(convert(u"\xDBFF\x0061") == u8"�a");
+    CHECK(convert(u"\xD800\xDC00") == u8"𐀀");
+    CHECK(convert(u"\xD800\xDFFF") == u8"\U000103FF");
+    CHECK(convert(u"\xDBFF\xDC00") == u8"\U0010FC00");
+    CHECK(convert(u"\xDBFF\xDFFF") == u8"\U0010FFFF");
+}
+
+TEST_CASE( "utf16 to utf32", "[utf]" )
+{
+    CHECK(convert32(u"κόσμε") == U"κόσμε");
+    CHECK(convert32(u"\xD800") == U"�");
+    CHECK(convert32(u"\xDBFF") == U"�");
+    CHECK(convert32(u"\xDC00") == U"�");
+    CHECK(convert32(u"\xDC00") == U"�");
+    CHECK(convert32(u"\xDFFF") == U"�");
+    CHECK(convert32(u"\xD800\xDBFF") == U"��");
+    CHECK(convert32(u"\xD800\x0061") == U"�a");
+    CHECK(convert32(u"\xDBFF\xDBFF") == U"��");
+    CHECK(convert32(u"\xDBFF\x0061") == U"�a");
+    CHECK(convert32(u"\xD800\xDC00") == U"𐀀");
+    CHECK(convert32(u"\xD800\xDFFF") == U"\U000103FF");
+    CHECK(convert32(u"\xDBFF\xDC00") == U"\U0010FC00");
+    CHECK(convert32(u"\xDBFF\xDFFF") == U"\U0010FFFF");
+}
+
+TEST_CASE( "utf32 to utf16", "[utf]" )
+{
+    CHECK(convert32(U"κόσμε") == u"κόσμε");
+    CHECK(convert32(U"𐀀") == u"\xD800\xDC00");
+    CHECK(convert32(U"\U000103FF") == u"\xD800\xDFFF");
+    CHECK(convert32(U"\U0010FC00") == u"\xDBFF\xDC00");
+    CHECK(convert32(U"\U0010FFFF") == u"\xDBFF\xDFFF");
+}
